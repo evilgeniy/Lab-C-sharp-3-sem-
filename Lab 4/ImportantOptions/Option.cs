6bb6be52a32f93da6e70b81a296fc5f42d1a1f27
evilgeniy <62379284+evilgeniy@users.noreply.github.com>
2020-12-18 00:27:09 +0300

    Add files via upload

---
+++ Lab 4/ImportantOptions/Option.cs
@@ -0,0 +1,31 @@
+﻿namespace ImportantOptions
+{
+    public class Option
+    {
+        public DirectoryOptions DirectoryOptions { get; set; }
+        public ArchivationOptions ArchivationOptions { get; set; }
+        public ServiceOptions ServiceOptions { get; set; }
+
+    }
+
+    public class DirectoryOptions
+    {
+        public string SourceDirectory { get; set; }
+        public string TargetDirectory { get; set; }
+    }
+
+    public class ArchivationOptions
+    {
+        public string ZipName { get; set; }
+    }
+
+    public class ServiceOptions
+    {
+
+        public bool CanStop { get; set; }// = true;
+
+        public bool CanPauseAndContinue { get; set; }// = true;
+
+        public bool AutoLog { get; set; }// = true;
+    }
+}
