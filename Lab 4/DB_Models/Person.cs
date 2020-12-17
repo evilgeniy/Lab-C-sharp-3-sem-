2bda9e9c09554ffdd2fdf77e02b67296dc97f41f
evilgeniy <62379284+evilgeniy@users.noreply.github.com>
2020-12-18 00:30:01 +0300

    Add files via upload

---
+++ Lab 4/DB_Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DB_Models
+{
+    [Serializable]
+    public class Person
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string City { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
---
+++ Lab 4/DB_Models/SearchModels/SearchCriteria.cs
@@ -0,0 +1,9 @@
+namespace DB_Models.SearchModels
+{
+    public class SearchCriteria
+    {
+        public int Page { get; set; }
+        public int Count { get; set; }
+        public int ID { get; set; }
+    }
+}
